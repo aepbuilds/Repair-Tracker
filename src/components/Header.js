8b12a3ac90bacf269e07af5afbbc0befb99d87d8
Jeanvally Beato <aephomthavong@gmail.com>
2022-03-25 08:24:19 -0700

    added design to Posts page

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,41 +3,42 @@ import { Link } from "react-router-dom";
 import UserContext from "../context/UserContext";
 
 const Header = (props) => {
+  const { user, onLogout } = useContext(UserContext);
 
-    const { user, onLogout } = useContext(UserContext);
-
-    return (
-        <header className="App-header">
-            <ul className="container">
-                <li key="home">
-                    <Link to="/">AEPBUILDS</Link>
-                </li>
-                {user.isAuthenticated ? (
-                    <>
-                        <li>
-                            <Link to="/new">New Repair</Link>
-                        </li>
-                        <li>
-                            <button
-                                className="linkLike"
-                                onClick={(event) => {
-                                    event.preventDefault();
-                                    onLogout();
-                                }}
-                            >
-                                Logout
-                            </button>
-                        </li>
-                        <p>Logged in as: {user.email}</p>
-                    </>
-                ) : (
-                    <li>
-                        <Link to="/login">Login</Link>
-                    </li>
-                )}
-            </ul>
-        </header>
-    );
-};  
+  return (
+    <header className="App-header">
+      <ul className="container">
+        <li key="home" className="link-home">
+          <Link to="/">Repair Tracker</Link>
+        </li>
+        {user.isAuthenticated ? (
+          <>
+            <li>
+              <Link to="/new">Add New Repair</Link>
+            </li>
+            <li>
+              <button
+                className="linkLike"
+                onClick={(event) => {
+                  event.preventDefault();
+                  onLogout();
+                }}
+              >
+                Logout
+              </button>
+            </li>
+            <div className="user-email">
+              <p>Logged in as: {user.email}</p>
+            </div>
+          </>
+        ) : (
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        )}
+      </ul>
+    </header>
+  );
+};
 
 export default Header;
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -4,34 +4,31 @@ import { Link } from "react-router-dom";
 import UserContext from "../context/UserContext";
 
 const Posts = ({ posts, deletePost }) => {
-    
-    const { user } = useContext(UserContext);
-    return (
-        <article className="posts container">
-        <h1>Repairs</h1>
-        <ul>
-            {posts.length < 1 && (
-                <li key="empty">No Posts yet!</li>
+  const { user } = useContext(UserContext);
+  return (
+    <main className="posts container">
+      <h1>Repairs</h1>
+      <ol className="repair-list">
+        {posts.length < 1 && <li key="empty">No Posts yet!</li>}
+        {posts.map((post) => (
+          <li className="repair-post" key={post.key}>
+            <h2>
+              <Link to={`/post/${post.slug}`}>{post.title}</Link>
+            </h2>
+            {user.isAuthenticated && (
+              <p>
+                <Link to={`/edit/${post.slug}`}>Edit</Link>
+                {" | "}
+                <button className="linkLike" onClick={() => deletePost(post)}>
+                  Delete
+                </button>
+              </p>
             )}
-            {posts.map(post => (
-                <li key={post.key}>
-                    <h2>
-                        <Link to={`/post/${post.slug}`}>{post.title}</Link>
-                    </h2>
-                    {user.isAuthenticated && (
-                        <p>
-                            <Link to={`/edit/${post.slug}`}>Edit</Link>
-                            {" | "}
-                            <button className="linkLike" onClick={() => deletePost(post)}>
-                                Delete
-                            </button>
-                        </p>
-                    )}
-                </li>
-            ))}
-        </ul>
-    </article>
-    )  
+          </li>
+        ))}
+      </ol>
+    </main>
+  );
 };
 
 export default Posts;
